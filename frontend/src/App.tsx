4669182f893e3ec9a4f08d92385a6198f27f209b
james.e.alonzo.mil <james.e.alonzo.mil@army.mil>
2026-04-13 14:24:26 -0400

    reyh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router'
 import SearchPage from './pages/SearchPage'
 import SectionDetailPage from './pages/SectionDetailPage'
 import PersonDetailPage from './pages/PersonDetailPage'
+import ExplorerPage from './pages/ExplorerPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SearchPage />} />
+      <Route path="/explorer" element={<ExplorerPage />} />
       <Route path="/sections/:sectionID" element={<SectionDetailPage />} />
       <Route path="/people/:personID" element={<PersonDetailPage />} />
     </Routes>
